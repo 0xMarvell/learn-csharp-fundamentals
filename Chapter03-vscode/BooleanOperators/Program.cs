48f4052fb74d28964ece70a2535df16922bfe777
0xMarvell <rokemarvellous@gmail.com>
2023-07-24 22:21:47 +0100

    update

---
+++ Chapter03-vscode/BooleanOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿bool a = true;
+bool b = false;
+
+static bool DoStuff() {
+    WriteLine("I am doing stuff");
+    return true;
+}
+
+WriteLine();
+WriteLine($"a & DoStuff = {a & DoStuff()}");
+WriteLine($"b & DoStuff = {b & DoStuff()}");
+
+WriteLine();
+WriteLine($"a && DoStuff() = {a && DoStuff()}");
+WriteLine($"b && DoStuff() = {b && DoStuff()}");
---
+++ Chapter03-vscode/IterationStatements/Program.cs
@@ -0,0 +1,39 @@
+﻿// int x = 0;
+// while (x <= 10) {
+//     WriteLine(x);
+//     x++;
+// }
+
+// string? password;
+// int attempts = 0;
+// do
+// {
+//     Write("Enter your password: ");
+//     password = ReadLine();
+//     attempts++;
+// } while (password != "pa$$word" && attempts < 10);
+// if (attempts >= 10)
+// {
+//     WriteLine("Too many attempts");
+// }
+// else
+// {
+//     WriteLine("Correct!");
+// }
+
+// for (int i = 0; i <= 10; i++)
+// {
+//     WriteLine(i);
+// }
+
+int[] nums = {2,44,5,1,6};
+foreach (int num in nums)
+{
+    WriteLine(num);
+}
+
+string[] names = { "Adam", "Barry", "Charlie" };
+foreach (string name in names)
+{
+WriteLine($"{name} has {name.Length} characters.");
+}
