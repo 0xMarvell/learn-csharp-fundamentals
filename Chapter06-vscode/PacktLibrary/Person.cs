4fff57e9af23682f304877c12a0301b3b8397e04
0xMarvell <rokemarvellous@gmail.com>
2023-08-19 08:22:11 +0100

    chapter 6

---
+++ Chapter06-vscode/PacktLibrary/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace Packt.Shared;
+
+public class Person : object
+{
+    // properties
+    public string? Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    // event handler
+    public event EventHandler? Shout;
+    public int AngerLevel;
+
+    // methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+    public void Poke()
+    {
+        AngerLevel++;
+        if (AngerLevel >= 3)
+        {
+            // if something is listening..
+            if (Shout != null)
+            {
+                // then call the delegate
+                Shout(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Chapter06-vscode/PeopleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using Packt.Shared;
+
+Person grundy = new()
+{
+    Name = "Solomon Grundy",
+    DateOfBirth = new(year: 2023, month: 7, day: 31)
+};
+
+grundy.WriteToConsole();
+
+// Dictionary<int, string> lookupObject = new();
+// lookupObject.Add(key: 1, value: "Alpha");
+// lookupObject.Add(key: 2, value: "Beta");
+// lookupObject.Add(key: 3, value: "Gamma");
+// lookupObject.Add(key: 4, value: "Delta");
+
+grundy.Shout += Grundy_Shout;
+grundy.Shout += Grundy_Shout2;
+grundy.Poke();
+grundy.Poke();
+grundy.Poke();
+grundy.Poke();
